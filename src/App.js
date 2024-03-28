a056d3e86d34fdf108eba2fde2cb90c745853846
yjlee <81798918+y-00jin@users.noreply.github.com>
2024-03-28 17:52:21 +0900

    24.03.28 스타일 수정전

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Layout from 'components/Layout';
 import Router from 'components/Router';
 function App() {
 
-  return <Router/>;
+  return (
+    <>
+      <Layout>
+        <Router />
+      </Layout>
+    </>
+  )
+  
 }
 
 export default App;
