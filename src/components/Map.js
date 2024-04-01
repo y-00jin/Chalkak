7bbb9aeb9fb699d8c614b4e492d2b351ef4de0da
yjlee <81798918+y-00jin@users.noreply.github.com>
2024-04-01 10:50:29 +0900

    24.04.01 Map 컴포넌트 생성

---
+++ src/components/Map.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react';
+
+// 기본 위도 경도 설정
+const DEFAULT_LAT = 37.497625203;
+const DEFAULT_LNG = 127.03088379;
+
+export default function Map({ setMap }) {
+
+    // 카카오 맵 로드
+    const loadKaKaoMap = () => {
+        window.kakao.maps.load(() => {
+            const mapContainer = document.getElementById("map");    // 지도 렌더링 할 요소 선택
+            
+            // 지도 옵션 설정
+            const mapOption = {
+                center: new window.kakao.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG), // 지도 중심점
+                level: 3,   // 확대 레벨
+            };
+            const map = new window.kakao.maps.Map(mapContainer, mapOption); // 지도 생성
+            setMap(map);
+        })
+    }
+
+    // 컴포넌트 마운트 시 실행
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_API_KEY}&autoload=false`;
+        script.async = true;
+        script.onload = loadKaKaoMap;
+        document.body.appendChild(script);
+
+        
+    }, [setMap]);
+
+    return (
+        <>
+            <div id="map" className="w-full h-screen"></div>
+        </>
+    )
+}
+
--- src/pages/map/index.js
+++ src/pages/map/index.js
@@ -1,63 +1,13 @@
-// mapTest.js
+import Map from 'components/Map';
+import React, { useEffect, useState } from 'react';
 
-import React, { useEffect } from 'react';
+export default function MapHome() {
 
-function MapHome() {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.async = true;
-    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_API_KEY}&autoload=false`;
-    document.head.appendChild(script);
-
-    script.onload = () => {
-      window.kakao.maps.load(() => {
-        const container = document.getElementById('map');
-        const options = {
-          center: new window.kakao.maps.LatLng(37.5665, 126.9780),
-          level: 3,
-        };
-
-        new window.kakao.maps.Map(container, options);
-      });
-    };
-
-    return () => {
-      document.head.removeChild(script);
-    };
-  }, []);
+  const [map, setMap] = useState(null);
 
   return (
     <div>
-      <div id="map" style={{ width: '100vw', height: '400vh' }}></div>
+      <Map setMap={setMap}/>
     </div>
   );
 }
-
-export default MapHome;
-
-
-
-
-
-// import React from 'react';
-// import { Map, MapMarker } from "react-kakao-maps-sdk";
-// export default function MapHome() {
-//   return (
-// <div>
-//       <Map
-//         center={{ lat: 37.506320759000715, lng: 127.05368251210247 }}
-//         style={{
-//           width: '100vw',
-//           height: '100vh',
-//           borderRadius: '20px',
-//         }}
-//       >
-
-// <MapMarker
-//           position={{ lat: 37.506320759000715, lng: 127.05368251210247 }}
-//         />
-        
-//       </Map>
-//     </div>
-//   );
-// }
